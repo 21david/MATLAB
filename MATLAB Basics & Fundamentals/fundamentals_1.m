f69d70887312fed5a4eca2ac4ab0f2070ae9f07e
David <david.espinosa.21@gmail.com>
2020-08-06 00:18:38 -0500

    Create fundamentals_1.m

---
+++ MATLAB Basics & Fundamentals/fundamentals_1.m
@@ -0,0 +1,37 @@
+% Any value you type gets printed to the "Command Window"
+5
+10
+201
+-234
+-432.5
+2147483649.1234
+'Strings'
+"Strings"
+[4 81 15 4 -3 0 13 -9 8 4 -5 2 0 12 4]
+[]
+[-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+
+% Adding a ';' at the end suppresses any output (nothing will show up in
+% the Command Window)
+5;
+12.69;
+420.69;
+"Just a random string";
+
+% Do a "Hello World"
+disp("Hello world!! :)");
+disp("Hello " + "world" + "!! " + 420.69);
+disp("Much like println() in java. " + "concatenate nums: " + (5*5^2));
+
+% variables:
+a = 17;
+b = 5;
+c = a^2 + b^2;
+disp(c);
+
+array = [420 69 1337 666 1234 169 42]
+
+matrix = [1 2 3 4;
+          5 6 7 8;
+          9 0 1 2;
+          3 4 5 6]
