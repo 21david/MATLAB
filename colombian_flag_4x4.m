795ac137aa1b21a272aa6ebde7fe73033432d014
David <david.espinosa.21@gmail.com>
2020-08-06 00:27:43 -0500

    Add files via upload

---
+++ colombian_flag_4x4.m
@@ -0,0 +1,32 @@
+Y = [255 255 0];
+B = [0 0 255];
+R = [255 0 0];
+
+F = 255;
+
+colombia_flag = zeros(4, 4, 3);
+
+% adding red
+for r = 1 : 4
+    for c = 1 : 4
+        colombia_flag(r, c, 1) = F;
+        if r == 3
+            colombia_flag(r, c, 1) = 0;
+        end
+    end
+end
+
+% adding green
+for r = 1 : 2
+    for c = 1 : 4
+        colombia_flag(r, c, 2) = F;
+    end
+end
+
+% adding blue
+for c = 1 : 4
+    colombia_flag(3, c, 3) = F;
+end
+
+imshow(colombia_flag);
+imwrite(colombia_flag, 'C:\Users\David\Downloads\Digital Image Processing\Matlab files\My files\colombia flag 4x4.bmp');
